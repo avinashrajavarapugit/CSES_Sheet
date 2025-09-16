391292e48f52cba52544386e273309360b588df9
Avinash Rajavarapu <rajavarapu.avinash@gmail.com>
2025-09-16 20:02:44 +0530

    Add files via upload

---
+++ all (1).py
@@ -0,0 +1,29 @@
+# src: Hi, I'm Avinash Rajavarapu
+
+import sys
+from collections import defaultdict, deque, Counter
+from bisect import bisect_left, bisect_right
+from heapq import heappush, heappop
+from math import gcd, sqrt, ceil, floor, inf
+from functools import lru_cache
+
+def ri(): return int(input())
+def rl(): return list(map(int, input().split()))
+def rs(): return input().strip()
+def rm(): return map(int, input().split())
+
+def solve():
+    n = ri()
+    a = rl()
+    sm = sum(a)
+    tot = (n * (n + 1)) // 2
+    print(tot - sm)
+            
+
+def run():
+    cases = 1
+    for _ in range(cases):
+        solve()
+
+if __name__ == "__main__":
+    run()
